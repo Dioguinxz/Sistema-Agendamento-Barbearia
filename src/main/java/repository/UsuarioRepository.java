cc0ada91c30ce44d58ecf1af1f9a38169653f9ee
Dioguinxz <doassis@hotmail.com.br>
2025-08-21 19:54:53 -0300

    Criacao da interface UsuarioRepository

---
+++ src/main/java/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+import entity.Usuario;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends MongoRepository<Usuario, String> {
+    Usuario findByEmail(String email);
+}
