7ad4d5e019dd002bc4cb318fd8939fdedd9592de
Dioguinxz <doassis@hotmail.com.br>
2025-08-21 18:53:56 -0300

    First commit

--- src/main/java/com/example/SistemaBarbearia/SistemaBarbeariaApplication.java
+++ src/main/java/com/example/SistemaBarbearia/SistemaBarbeariaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SistemaBarbeariaApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaBarbeariaApplication.class, args);
 	}
